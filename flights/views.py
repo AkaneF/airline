93a592de948dcabaaf5b2535e220c8b43aa39e08
akane fukui <akane.1418@gmail.com>
2020-04-24 11:55:07 +0200

    booking added

--- flights/views.py
+++ flights/views.py
@@ -19,7 +19,23 @@ def flight(request, flight_id):
         raise Http404("Flight does not exist.")
     context = {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+        #exclude all the passenger have flight id already
     }
     return render(request, "flights/flight.html", context)
 
+def book(request, flight_id):
+  try:
+      passenger_id = int(request.POST["passenger"])
+      passenger = Passenger.objects.get(pk=passenger_id)
+      flight = Flight.objects.get(pk=flight_id)
+  except KeyError:
+      return render(request, "flights/error.html", {"message": "No selection"})
+  except Flight.DoesNotExist:
+      return render(request, "flights/error.html", {"message": "No flights."})
+  except Passenger.DoesNotExist:
+      return render(request, "flights/error.html", {"message": "No passenger."})
+
+  passenger.flights.add(flight)
+  return HttpResponseRedirect(reverse("flight", args=(flight_id)))
